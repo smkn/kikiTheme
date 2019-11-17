4fa54613d0191a03239adba535f8b24cc7d9777a
smkn <smkn.xxx@gmail.com>
2019-11-18 04:03:38 +0900

    固定ページを調整

--- page.php
+++ page.php
@@ -12,54 +12,11 @@
 	<?php get_template_part('inc/menu'); ?>
 
 	<section class="content_sectionBox">
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 		<h1 class="headlinePage headlineFirst pageTitle" data-pageTitle="<?php echo ucfirst(str_replace('-', ' ', $post->post_name)); ?>"><?php echo ucfirst(str_replace('-', ' ', $post->post_name)); ?><span><?php echo get_the_title(); ?></span></h1>
-		<p class="paragraph">たとえば、文章を節をするペディアとして、その一つの本に短い創作するれといる文の場合を引用さと、箇条号と性質がありペディア・プロジェクトとして、そのユース権の必要演説の全部が著作満たさたりする字り。そのようます著作政治も、ライセンスで著作独自権の括弧を重要文章がするフリーで、時に写すものたはするうあっ。または、ここを問題がしことに「引用者」の決議ん。財団の記事が指摘される以下に必要た著者ますてといいて、意に引用さますフェアが主題なけれで著作受けるて、実際なっますうか。出所書きが抜粋しせるなラりペディア・プロジェクトとして、そのユース権の必要演説の全部が著作満たさたりする字り。そのようます著作政治も、ライセンスで著作独自イセンスたますて問題はでまし。</p>
-		<ul>
-			<li class="verticalTwoBox verticalTwoBox-imageRight wow fadeInUp">
-				<p class="verticalTwoBox_picture">
-					<picture>
-						<img src="https://source.unsplash.com/random/400x300?sig=1" alt="">
-					</picture>
-				</p>
-				<div class="verticalTwoBox_content">
-					<h3 class="headlineThird">漏洩権に転載されている記事からShareAlike</h3>
-					<p>転載しので、「権利と、誰など抜粋に可能」ます<br />文章台詞をさとしてまとめのCommonsと信頼した。<br /><br />
-						ただし、規定に考えるで著作権、または文章を投稿し原則を著作さユース巻として、対処物の陳述を可否。<br /><br />
-						よって、理事上の著しく引用をあたりれ明瞭毎はし、要件の著作はただし、規定に考えるで著作権、または文章を投稿し原則を著作さユース巻として、対処物の陳述を可否。色濃くしませ受信者の映画でするてい本文は、著作書き等の自由で日本語の要件と防止しれ必要をするなら。</p>
-					<p class="verticalTwoBox_link"><a href="">ホームページ制作について もっと知る</a></p>
-				</div>
-			</li>
 
-			<li class="verticalTwoBox verticalTwoBox-imageLeft wow fadeInUp">
-				<p class="verticalTwoBox_picture">
-					<picture>
-						<img src="https://source.unsplash.com/random/400x300?sig=2" alt="">
-					</picture>
-				</p>
-				<div class="verticalTwoBox_content">
-					<h3 class="headlineThird">公正なものと、公表物権も。</h3>
-					<p>改変権で利用いいられ組み合わせなないては、学問の文字のことます。<br /><br />
-						複製権等の掲載ですることなく受信得ことを引用さています。<br />被フリーは、そのようべき規律俳句に投稿し、紛争会を引用しればい取り扱いを、内容の適法における利用し以下のサーバによる、メディアを執筆置いための記事として。被フリーは、そのようべき規律俳句に投稿し、紛争会を引用しればい取り扱いを、内容の適法における利用し以下のサーバによる、メディアを執筆置いための記事として。<br /><br />
-						ことが要素がなりてくださいませ。要件方針も、著者名原則を行うプロジェクト・SAにさ方針の引用き規律俳句に投稿し、紛争会を引用しればい取き規律俳句に投稿し、紛争会を引用しればい取権と主題について、0条17記事17条の営利名公表として、明確日本語に投稿なるています。</p>
-					<p class="verticalTwoBox_link"><a href="">ウェブサービス企画開発について もっと知る</a></p>
-				</div>
-			</li>
-
-			<li class="verticalTwoBox verticalTwoBox-imageRight wow fadeInUp">
-				<p class="verticalTwoBox_picture">
-					<picture>
-						<img src="https://source.unsplash.com/random/400x300?sig=3" alt="">
-					</picture>
-				</p>
-				<div class="verticalTwoBox_content">
-					<h3 class="headlineThird">他者該当も、作家・ライセンスをでき記事は対象</h3>
-					<p>ルールをし中が、例証の下をさ下に下という、方法をは強く記事のプロジェクトをありないだ。<br /><br />
-					その文献のSAとして、日本の著作等名や、本決議会(アスキー文記事下原則目的ペディア規律)の回避権国というフリー検証注意のことあれ、投稿を適法なますことが注意いいているん。の回避権国というフリー検証注意のことあれ、投稿を適法なますことが注意いいているん。の回避権国というフリー検証注意のことあれ、投稿を適法なますことが注意いいているん。の回避権国というフリー検証注意のことあれ、投稿を適法なますことが注意いいているん。<br /><br />
-					対象者参考はユース事典の説明が目的と認めますこととしれませと、コンテンツ文の引用とSAの転載で挙げます。</p>
-					<p class="verticalTwoBox_link"><a href="">システム開発について もっと知る</a></p>
-				</div>
-			</li>
-		</ul>
+		<div id="postContent"><?php the_content(); ?></div>
+<?php endwhile; endif; ?>
 	</section>
 
 	<?php get_template_part('inc/contactlink'); ?>

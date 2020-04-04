6943d2b0281fefc06bef1f1b58fec9ba22834a6b
smkn <smkn.xxx@gmail.com>
2020-04-05 05:03:09 +0900

    OGP周りの不具合修正

--- functions/_custom.php
+++ functions/_custom.php
@@ -3,6 +3,47 @@
 
 
 
+// OGP出力 https://saruwakakun.com/html-css/wordpress/ogp
+function my_meta_ogp($ogp_img = false) {
+	$ogp_title = get_bloginfo('description').' | '.get_bloginfo('name');
+	$ogp_descr = '「'.get_bloginfo('name').'」は、'.get_bloginfo('description').'です。私たちは「人には優しく、技術には貪欲に、課題には丁寧に」寄り添うことを命題に掲げ、クライアント、ステークホルダー、株主、従業員、ユーザー、そして私たち自身も、関係するすべての人に幸せを提供し、それを分かち合える会社でありたいと願っています。';
+	$ogp_url = home_url('/');
+	$ogp_img = get_template_directory_uri().'/img/ogp.png';
+
+	if (is_page()) {
+		$ogp_title = get_the_title().' | '.get_bloginfo('name');
+		$ogp_descr = get_the_title();
+		$ogp_url = get_permalink();
+	} elseif(is_post_type_archive()) {
+		$ogp_title = esc_html(get_post_type_object(get_post_type())->label).' | '.get_bloginfo('name');
+		$ogp_descr = get_bloginfo('name').'の'.esc_html(get_post_type_object(get_post_type())->label).'ページです。';
+		$ogp_url = home_url('/').get_post_type_object(get_post_type())->name.'/';
+	} elseif(is_single()) {
+		global $post;
+		$ogp_title = get_the_title().' | '.get_bloginfo('name');
+		$ogp_descr = get_the_excerpt().' | '.get_bloginfo('name');
+		$ogp_url = get_permalink();
+		$ogp_img = get_the_post_thumbnail_url($post->ID, 'thumb_ogp');
+	}
+
+	$ogp_type = ( is_front_page() || is_home() ) ? 'website' : 'article';
+	$ogp_site_name = get_bloginfo('description').' | '.get_bloginfo('name');
+	$insert = '';
+	$insert .= '<meta property="og:locale" content="ja_JP">' . "\n";
+	$insert .= '<meta property="og:type" content="'.$ogp_type.'">' . "\n";
+	$insert .= '<meta property="og:site_name" content="'.esc_attr($ogp_site_name).'">' . "\n";
+	$insert .= '<meta property="og:title" content="'.esc_attr($ogp_title).'">' . "\n";
+	$insert .= '<meta property="og:description" content="'.esc_attr($ogp_descr).'">' . "\n";
+	$insert .= '<meta property="og:url" content="'.esc_url($ogp_url).'">' . "\n";
+	$insert .= '<meta property="og:image" content="'.esc_url($ogp_img).'">' . "\n";
+	$insert .= '<meta name="twitter:card" content="summary_large_image">' . "\n";
+
+	echo $insert;
+}
+add_action('wp_head', 'my_meta_ogp');
+
+
+
 // カスタムメニュー機能の導入
 add_theme_support('menus');
 //// カスタムメニューの初期設定
